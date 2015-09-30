a12284a146bdb579a8cfdd230f2efa4450712272
eliot.andres <eliot.andres@eleves.enpc.fr>
2015-09-30 22:25:58 +0200

    Add smooth scroll and nice menu

--- js/main.js
+++ js/main.js
@@ -1,5 +1,12 @@
 jQuery(document).ready(function($){
-
+	
+	$('.scrollTo').click( function() { // Au clic sur un élément
+		var page = $(this).attr('href'); // Page cible
+		var speed = 750; // Durée de l'animation (en ms)
+		$('html, body').animate( { scrollTop: $(page).offset().top }, speed ); // Go
+		return false;
+	});
+	
 	var enableGallery = function() {
 			$('.zoom-gallery').magnificPopup({
 			delegate: 'a',
